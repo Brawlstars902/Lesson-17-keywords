f9d160a3a3cf49dde7d8eaf57034a7d1b2329698
Brawlstars902 <aparna.rautela@gmail.com>
2025-11-09 07:07:35 +0530

    Add files via upload

---
+++ Pass.py
@@ -0,0 +1,10 @@
+for x in range(1,100):
+    if x==3:
+        pass
+    if x==10:
+        print('\'10\'')
+        continue
+    if x==67:
+        print('67676767')
+    if x%1==0:
+        print(x)
